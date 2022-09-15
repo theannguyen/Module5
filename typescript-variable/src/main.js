d0f388eb7fc152c769584c6814538ae81cc6473a
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-09-15 08:41:32 +0700

    thực hành 2 - bài 1

---
+++ typescript-variable/src/main.js
@@ -0,0 +1,18 @@
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+//
+// if (environment.production) {
+//   enableProdMode();
+// }
+//
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+var width;
+var height;
+width = 10.5;
+height = 20;
+var area = width * height;
+console.log("Di\u1EC7n t\u00EDch h\u00ECnh ch\u1EEF nh\u1EADt: ".concat(area));
---
+++ typescript-variable/src/main.ts
@@ -0,0 +1,21 @@
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+//
+// if (environment.production) {
+//   enableProdMode();
+// }
+//
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+let width: number;
+let height: number;
+
+width = 10.5;
+height = 20;
+
+let area: number = width * height;
+console.log(`Diện tích hình chữ nhật: ${area}`);
