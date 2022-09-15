fc5f14ee31371794174dc09a9c977524e6c70c79
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-09-15 09:10:51 +0700

    thực hành 2 - bài 2

---
+++ typescript-control-flow-and-loop/src/main.js
@@ -0,0 +1,34 @@
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+//
+// if (environment.production) {
+//   enableProdMode();
+// }
+//
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+var sum = 0;
+var count = 0;
+for (var i = 2; count < 30; i++) {
+    var isPrime = true;
+    if (i == 2) {
+        sum += i;
+        count++;
+        continue;
+    }
+    for (var j = 2; j <= Math.sqrt(i); j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (!isPrime) {
+        continue;
+    }
+    sum += i;
+    count++;
+}
+console.log(sum);
---
+++ typescript-control-flow-and-loop/src/main.ts
@@ -0,0 +1,34 @@
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+//
+// if (environment.production) {
+//   enableProdMode();
+// }
+//
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+let sum: number = 0;
+let count: number = 0;
+for (let i = 2; count < 30; i++) {
+  let isPrime: boolean = true;
+  if (i == 2) {
+    sum += i;
+    count++;
+    continue;
+  }
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if(!isPrime){
+    continue;
+  }
+  sum += i;
+  count++;
+}
+console.log(sum);
