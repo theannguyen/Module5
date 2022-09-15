93b7bd57c922e85c0e698526127f41cda0c6b39d
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-09-15 14:34:45 +0700

    thực hành 2 - bài 4

---
+++ typescript-is-prime2/src/rectangle.ts
@@ -0,0 +1,19 @@
+import {Shape} from "./shape";
+
+export class Rectangle extends Shape implements IArea{
+  constructor(x: number, y: number, private _width: number, private _height: number) {
+    super(x, y);
+  }
+
+  area(): number {
+    return this._width*this._height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  set width(width: number) {
+    this._width = width;
+  }
+}
